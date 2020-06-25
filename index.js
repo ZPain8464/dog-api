d65280475615c4479c9b58b4d4f5f38640b6da9a
Zach Painter <z.painter45@gmail.com>
2020-06-25 12:24:44 -0500

    fixed up html and js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 
 
-// function getDogImage() {
-//     console.log('get DogImage ran')
-//     let URL = "https://dog.ceo/api/breeds/image/random";
-//     $(`/${numImages}`).appendTo(URL);
-//     fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json())
-//     .then(responseJson => 
-//         displayResults(responseJson))
-//         // append 
-//     .catch(error => alert('Something went wrong. Try again later.'));
-// }
-
 function displayResults(responseJson) {
-    console.log(responseJson)
-    $('.results').replaceWith(
-        `<img src="${responseJson.message}">`
-    );
+    console.log('displayResults ran');
+    console.log(responseJson.message);
+    for (let i = 0; i < responseJson.message.length; i++) {
+    $('.js-images').append(`
+    <li><img src="${responseJson.message[i]}"></li><hr>`);
+    };
+    
+
 }
+
 
 function watchForm() {
     console.log('watchForm ran')
     $('form').submit(event =>{
         event.preventDefault();  
         numImages = $('#text-box').val();
-        // console.log(numImages)
-        // getDogImage();
         let URL = `https://dog.ceo/api/breeds/image/random/${numImages}`;
-    // $(`/${numImages}`).appendTo(URL);
     fetch(URL).then(response => response.json())
     .then(responseJson => 
         displayResults(responseJson))
-        // append 
     .catch(error => alert('Something went wrong. Try again later.'))
     });
 }
